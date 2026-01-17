9a104ddbda122e1ccbe7f873fc5a579b2e4be11f
Jan Marthedal Rasmussen <jan@janmr.com>
2026-01-17 13:49:12 +0100

    Fix rust install

---
+++ configs/rust/50-cargo.sh
@@ -0,0 +1,2 @@
+# Update path to include rust tools
+. "$HOME/.cargo/env"
--- install/rust.sh
+++ install/rust.sh
@@ -1,9 +1,8 @@
-sudo apt remove -y rustc libstd-rust-dev
-
 cd /tmp
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs >rustup.sh
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs > rustup.sh
 sh rustup.sh -y --no-modify-path
 rm rustup.sh
 cd -
 
-cp ~/.local/share/my-linux-setup/configs/rust/*.sh ~/.config/my-linux-setup/bashrc/
+cp ~/.local/share/my-os-setup/configs/rust/*.sh ~/.config/my-os-setup/zshrc/
+
